b3829f65aea79a1152016b58c4e1c09b43b8ff6a
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-02-02 22:57:13 +0100

    chore: one more alias

--- terminal/.config/fish/conf.d/alias-linux.fish
+++ terminal/.config/fish/conf.d/alias-linux.fish
@@ -5,9 +5,11 @@ switch (uname)
         alias btd='bluetoothctl disconnect'
         alias tarnow='tar -acf '
         alias untar='tar -zxvf '
+        # Cachyos based alias
         alias hw='hwinfo --short'                                   # Hardware Info
         alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
         alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
         alias mirror="sudo cachyos-rate-mirrors"
         alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+        alias jctl="journalctl -p 3 -xb"                            # Get the error messages from journalctl
 end
