000c89973e62da3c6f73aab4b9fd513c16838502
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-02-02 13:32:12 +0100

    chore: theme and icons are already present in cachy

--- taskfiles/scripts/50-gnome.sh
+++ taskfiles/scripts/50-gnome.sh
@@ -17,18 +17,3 @@ for PKG in "${PKGS[@]}"; do
 	echo "Installing package: $PKG"
 	sudo pacman -S "$PKG" --noconfirm --needed
 done
-
-YAY_PKGS=(
-	whitesur-gtk-theme
-	whitesur-icon-theme
-
-)
-
-echo "Installing yay packages"
-
-for YAY in "${YAY_PKGS[@]}"; do
-	echo "Installing package: $YAY"
-	yay -S "$YAY" --noconfirm --needed
-done
-
-echo "Packages installed"
